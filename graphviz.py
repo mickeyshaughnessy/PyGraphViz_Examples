417d9b5d951721b614f762e9712b7b5e665917e1
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-05-13 11:21:09 -0700

    fix graphviz.py 2

--- graphviz.py
+++ graphviz.py
@@ -6,7 +6,8 @@ if len(sys.argv) > 1:
     name = str(sys.argv[1]) 
 
 D=pgv.AGraph(name)
-D.layout('dot') # layout with dot 
+D.layout('dot') # layout with dot
+name = name[:-4]
 D.draw(name+'.png') # draw png
 print("Wrote "+name+".png")
 
