c680e2b671fffee232df48ff5ba37cab85a62d36
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-05-13 11:18:35 -0700

    fix graphviz.py

--- graphviz.py
+++ graphviz.py
@@ -1,11 +1,11 @@
 import pygraphviz as pgv
 import sys
 
-name = 'MDDFT'
+name = 'MDDFT.dot'
 if len(sys.argv) > 1:
     name = str(sys.argv[1]) 
 
-D=pgv.AGraph(name+'.dot')
+D=pgv.AGraph(name)
 D.layout('dot') # layout with dot 
 D.draw(name+'.png') # draw png
 print("Wrote "+name+".png")
